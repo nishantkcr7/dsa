ec0c87409f4a556548d32db5e6b532dde30e63df
Nishant <nishantkcr7@gmail.com>
2022-12-26 11:20:38 +0530

    removed exe files

--- Section 5 - Recursion/62_sum_of_n_natural_number.cpp
+++ Section 5 - Recursion/62_sum_of_n_natural_number.cpp
@@ -1,17 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int sumOfNaturalNumberUpTo(int num){
-    if(num == 0)
+int sumOfNaturalNumberUpTo(int num)
+{
+    if (num == 0)
         return 0;
     else
-        return sumOfNaturalNumberUpTo(num-1) + num;
+        return sumOfNaturalNumberUpTo(num - 1) + num;
 }
-int main(){
+int main()
+{
     int n;
     cout << "Enter desired sum up to number: ";
     cin >> n;
     int result = sumOfNaturalNumberUpTo(n);
-    cout << endl << "Sum of natural number upto " << n << " : "<< result;
+    cout << endl
+         << "Sum of natural number upto " << n << " : " << result;
 
     return 0;
 }
